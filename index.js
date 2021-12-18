be065186b86f15770cc0a29ad623d4c17a738868
Gianluca Cavajani <cavajanig@gmail.com>
2021-12-18 19:57:50 -0300

    ex 3.11 fixed

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors);
 app.use(express.static("build"));
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
